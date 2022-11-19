94421219504a9158d685b77a3a957ea295e4d0ef
ShishirShekhar <sspdav02@gmail.com>
2022-11-19 14:57:43 +0530

    hosting

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 dotenv.config()
 
 // connect mongoDB
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wbjmy.mongodb.net/blogDB?retryWrites=true&w=majority`);
+mongoose.connect(process.env.URL);
 
 // add some content for different pages.
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
